dfda6f3503ddc7a828fef545e7ea5c388f82483c
Andrew <45402766+Andrik154@users.noreply.github.com>
2021-09-22 01:18:03 +0300

    Update tests-front.js

--- routers/tests-front.js
+++ routers/tests-front.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.use(express.static(path.join(__dirname, 'tests-react/build')));
-app.get('/', function (req, res) {
+router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
